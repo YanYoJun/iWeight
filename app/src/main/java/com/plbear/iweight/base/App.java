6564e3a4c32a5f5e05cae81b140f127bad66659f
yanyongjun <yanyojun@foxmail.com>
2018-09-06 21:58:05 +0800

    Bug修复

--- app/src/main/java/com/plbear/iweight/base/App.java
+++ app/src/main/java/com/plbear/iweight/base/App.java
@@ -6,6 +6,7 @@ import android.net.Network;
 
 import com.plbear.iweight.data.NetworkDataManager;
 import com.plbear.iweight.utils.Utils;
+import com.tencent.bugly.Bugly;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -22,7 +23,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         mContext = getApplicationContext();
-        CrashReport.initCrashReport(getApplicationContext(),Constant.BUGLY_ID, Utils.DEBUG);
+        //CrashReport.initCrashReport(getApplicationContext(),Constant.BUGLY_ID, Utils.DEBUG);
+        Bugly.init(mContext,Constant.BUGLY_ID,Utils.DEBUG);
         NetworkDataManager.getsInstance().sync();
 
         super.onCreate();
